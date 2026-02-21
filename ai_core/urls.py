cf9b26f7d6c478e71bf17073cfdd43298a13eccf
Phạm Lê Ngọc Ngân <ngan.pham1409207@hcmut.edu.vn>
2026-02-22 03:00:37 +0700

    Delete UI upload for AI

--- ai_core/urls.py
+++ ai_core/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 app_name = "ai_core"
 
-urlpatterns = [
-    path("upload-center/", views.ai_upload_center, name="ai_upload_center"),
-    path("process-document/", views.process_ai_document, name="process_ai_document"),
+urlpatterns = [ 
     path("lesson/generate/", views.generate_lesson, name="generate_lesson"),
     path("quiz/generate/", views.generate_quiz, name="generate_quiz"),
 ]
